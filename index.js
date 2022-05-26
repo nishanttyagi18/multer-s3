7347640502a08e3887d0f915016f04d60caf4518
nishanttyagi18 <ntyagi893@gmail.com>
2022-05-26 13:49:21 +0530

    File filters and error handling

--- index.js
+++ index.js
@@ -5,19 +5,51 @@ const app = express();
 
 // custom filename
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "uploads/");
-    },
-    filename: function (req, file, cb) {
-        cb(null, `${uuid()}-${file.originalname}`);
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${uuid()}-${file.originalname}`);
+  },
+});
 
-const upload = multer({storage})
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.split("/")[0] === "image") {
+    cb(null, true);
+  } else {
+    cb(new multer.MulterError("LIMIT_UNEXPECTED_FILE"), false);
+    // cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 1024 * 1024 * 5 /* 5 MB */ },
+  files: 2,
+});
 
 app.post("/upload", upload.array("file"), (req, res) => {
   console.log(req.files); // we can see the metadata of files uploaded
   res.json({ status: "success" });
 });
+
+// error handling - it got triggered if any of our controller throws an error
+app.use((err, req, res, next)=>{
+    if(err instanceof multer.MulterError){
+        if(err.code === 'LIMIT_FILE_SIZE'){
+            return res.json({message: 'File size is too large'})
+        }
+
+        if(err.code === 'LIMIT_FILE_COUNT'){
+            return res.json({message: 'File Limit reached'})
+        }
+
+        if(err.code === 'LIMIT_UNEXPECTED_FILE'){
+            return res.json({message: 'Unexpected file'})
+        }
+        
+    }
+})
 
 app.listen(3000, () => console.log("running on 3000"));
