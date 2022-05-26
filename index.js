2503c725e083309f242594391618955f1fabd880
nishanttyagi18 <ntyagi893@gmail.com>
2022-05-26 13:21:47 +0530

    Multiple file upload with different fields

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const multer = require('multer');
+const express = require("express");
+const multer = require("multer");
 const app = express();
 
+const upload = multer({ dest: "uploads/" });
 
-const upload = multer({dest : "uploads/"})
+const multiUpload = upload.fields([
+  { name: "avatar", maxCount: 1 },
+  { name: "resume", maxCount: 1 },
+]);
 
-app.post('/upload',upload.array("file", 2), (req,res)=>{
-    res.json({status: 'success'})
-})
+app.post("/upload", multiUpload, (req, res) => {
+  console.log(req.files); // we can see the metadata of files uploaded
+  res.json({ status: "success" });
+});
 
-app.listen(3000,()=> console.log("running on 3000"))
+app.listen(3000, () => console.log("running on 3000"));
